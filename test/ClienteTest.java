a152a5f6a76f4470df6070058c3af089ba604ddf
Vinicius Lemes <viniciuslemescaetano@gmail.com>
2021-11-05 20:33:22 -0300

    cria teste para classe Cliente

--- test/ClienteTest.java
+++ test/ClienteTest.java
@@ -1,9 +1,24 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class ClienteTest {
 
     @Test
     public void deveInstanciarClienteComSucesso() {
+
+        Endereco enderecoVinicius = new Endereco("Sargento", "Vila Jardim", "91342005", "Porto Alegre", "RS");
+        Contato contatoVinicius = new Contato("vinicius@gmail.com", "555198465629");
+
+        Cliente vinicius = new Cliente(contatoVinicius, enderecoVinicius, "Vinicius", "77788860096");
+
+        Assertions.assertEquals(enderecoVinicius, vinicius.getEndereco());
+        Assertions.assertEquals(contatoVinicius, vinicius.getContato());
+        Assertions.assertEquals("Vinicius", vinicius.getNome());
+        Assertions.assertEquals("77788860096", vinicius.getCpf());
+    }
+
+    @Test
+    public void deveValidarInstanciaComTamanhoDeCamposInvalidos() {
 
 
     }
