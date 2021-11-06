88f33c5eb90aaabcfb83004abddfbdeda4a0ef08
Vinicius Lemes <viniciuslemescaetano@gmail.com>
2021-11-05 21:09:36 -0300

    Cria classe Helper para o teste da classe cliente

---
+++ test/Helper.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Helper {
+
+    public String gerarPalavra(int tamanho) {
+
+        List<String> caracteres = new ArrayList<>();
+        for (int i = 0; i <= 100) {
+
+            caracteres.add("a");
+        }
+        return (caracteres.toString());
+    }
+}
