0cac9a12e9093cb0a56f8a922b853b5a0b474f15
Vinicius Lemes <viniciuslemescaetano@gmail.com>
2021-11-10 21:30:31 -0300

    Cria classe Item e ItemTest

---
+++ src/Item.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item {
+
+    private Produto produto;
+    private int quantidade;
+
+    public Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        validar();
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double valorTotalItens() {
+
+        double total = this.produto.getValorUnitario() * this.quantidade;
+        return total;
+    }
+
+    public void validar() {
+
+        List<String> mensagensDeErro = new ArrayList<>();
+
+        if (this.produto == null) {
+
+            mensagensDeErro.add("Produto deve ser Informado!");
+        }
+        if (this.quantidade <= 0) {
+
+            mensagensDeErro.add("Deve haver pelo menos 1 item!");
+        }
+        if (!mensagensDeErro.isEmpty()) {
+
+            throw new IllegalArgumentException(mensagensDeErro.toString());
+        }
+    }
+}
