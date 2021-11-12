1127c6aa001a680cc3887c1f4a1b1746464c6b68
Vinicius Lemes <viniciuslemescaetano@gmail.com>
2021-11-12 17:01:19 -0300

    Finaliza teste de Item

---
+++ test/ItemTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    public void deveInstanciarItemComSucesso() {
+
+        Produto camiseta = new Produto("T-Shirt", 29.99);
+        Item item1 = new Item(camiseta, 5);
+
+        Assertions.assertEquals(camiseta, item1.getProduto());
+        Assertions.assertEquals(5, item1.getQuantidade());
+    }
+    @Test
+    public void deveValidarInstanciaDeItemComErro() {
+
+        try {
+
+            new Item(null, 0);
+        } catch (Exception exception) {
+
+            Assertions.assertEquals("[Produto deve ser informado!, Deve haver pelo menos 1 item!]", exception.getMessage());
+        }
+    }
+    @Test
+    public void deveValidarMetodoGetValorTotalItens() {
+
+        Produto camiseta = new Produto("T-Shirt", 29.99);
+        Item item1 = new Item(camiseta, 5);
+
+        Double valorTotal = item1.valorTotalItens();
+
+        Assertions.assertEquals(149.95, valorTotal);
+    }
+}
